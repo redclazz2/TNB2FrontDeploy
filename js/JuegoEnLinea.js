3cda0ff44110d0d4fd5d7576f23a2a2d574bee75
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2023-08-30 23:03:17 -0500

    First Commit

---
+++ js/JuegoEnLinea.js
@@ -0,0 +1,48 @@
+//Importa el cliente de colyseus
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+let client = new  Colyseus.Client('ws://game.thenexusbattles2.com:3000/'),
+     cookie_data= "";
+
+if(getCookie("config").includes('1')){
+    cookie_data = {
+        numero_creditos: getCookie("creditosValor"),
+        numero_jugadores: getCookie("numJugadores"),
+        tj2: getCookie("tj2"),
+        tj3: getCookie("tj3"),
+        tj4: getCookie("tj4"),
+        equipos: getCookie("equipos"),
+        ej1:getCookie("ej1"),
+        ej2:getCookie("ej2"),
+        ej3:getCookie("ej3"),
+        ej4:getCookie("ej4")
+    }
+
+    //console.log(cookie_data);
+    client.create("room_battle",cookie_data).then(room => {
+        console.log(room.sessionId, "joined", room.name);
+        const displayLog = document.getElementsByTagName('h3');
+        displayLog[0].innerHTML = `Conectado a:${room.sessionId}. Esperando jugadores ...`
+    }).catch(e => {
+        console.log("JOIN ERROR", e);
+    });
+}else if(getCookie("config").includes('2')){
+    //Codigo de unirse a una partida
+}else{
+  //Mostrar mensaje de error inesperado
+  console.error("Unable to resolve game-command! Please allow cookies in your browser!")
+}
---
+++ js/ListaPartidas.js
@@ -0,0 +1,74 @@
+let client = new Colyseus.Client('ws://game.tnb2testing.com:3000/');
+
+// Tabla en HTML donde se guardan las filas
+const table = document.querySelector(".tabla");
+
+const functionBusqueda = async () => {
+  try {
+    // Seleccionar todos los elementos con la clase "fila" y eliminarlos
+    const filas = document.querySelectorAll('.fila');
+    filas.forEach(row => row.remove());
+
+    await client.getAvailableRooms("room_battle").then(rooms => {
+      if (rooms.length > 0) {
+        rooms.forEach(room => {
+          console.log(room.roomId);
+          console.log(room.clients);
+          console.log(room.maxClients);
+          console.log(room.metadata);
+          // Div con clase fila donde se guardará la información de la partida
+          const tableItem = document.createElement("div");
+          tableItem.classList.add("fila");
+
+          tableItem.innerHTML = `
+            <div class="columna nombreP">${room.roomId}</div>
+            <div class="columna">${room.clients}/${room.maxClients}</div>
+            <div class="columna">${room.metadata.ganacia}</div>
+            <div class="columna"><button>Join</button></div>
+          `;
+          // Div fila se guarda en la tabla
+          table.appendChild(tableItem);
+        });
+      } else {
+        console.log("NO DATA");
+
+        // Crear el elemento HTML para el mensaje de error
+        const errorPopup = document.createElement("div");
+        errorPopup.classList.add("popup-container");
+        errorPopup.innerHTML = `
+          <div class="popup">
+              <div class="popup-header">
+                  <span class="error-icon">!</span>
+              </div>
+              <div class="popup-content">
+                  <p>¡Ups! Parece que no hay partidas aun. Por favor, intenta de nuevo más tarde.</p>
+                  <div class="btn__confirmar">
+                    <button class="close-button">Confirmar</button>
+                  </div>
+              </div>
+          </div>
+        `;
+
+        // Agregar el mensaje de error al cuerpo del documento
+        document.body.appendChild(errorPopup);
+
+        //Eliminar el mensaje de error al hacer click en "Confirmar"
+        const closeButton = document.querySelector(".close-button");
+        if (closeButton) {
+          closeButton.addEventListener("click", () => {
+            errorPopup.remove();
+          });
+        }
+      }
+    }).catch(e => {
+      console.log("JOIN ERROR", e);
+    });
+  } catch (error) {
+    console.error('Ocurrió un error:', error);
+  }
+};
+
+const boton_actualizar = document.getElementById('btn_actualizar');
+boton_actualizar.addEventListener('click', functionBusqueda);
+
+
